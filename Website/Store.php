bc47f05b3c96e6e1b8192b64229747bd3b4dc5b8
ShaunHarmon <shaun.harmon@gmail.com>
2018-10-20 23:35:45 -0500

    up

--- Website/Store.php
+++ Website/Store.php
@@ -27,13 +27,13 @@
         <?php 
             //Allow Headers 
             //header('Access-Control-Allow-Origin: *');
-            $servername = "localhost:3306"; 
-            //$servername = "sh7652server.centralus.cloudapp.azure.com:3306";
+            //$servername = "localhost:3306"; 
+            $servername = "sh7652server.centralus.cloudapp.azure.com:3306";
             $username = "manager1"; 
             $password = "July201998!!"; 
             $dbname = "products";
             // Create connection 
-            $conn = new mysqli($servername, $username, $password, $dbname);
+            $conn = mysqli_connect($servername, $username, $password, $dbname);
             
             // Check connection 
             if ($conn->connect_error) 
