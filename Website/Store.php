f99de1d96eff3217fe9ad471e500e98a2194da8a
ShaunHarmon <shaun.harmon@gmail.com>
2018-10-20 23:31:22 -0500

    up

--- Website/Store.php
+++ Website/Store.php
@@ -33,7 +33,7 @@
             $password = "July201998!!"; 
             $dbname = "products";
             // Create connection 
-            $conn = mysqli($servername, $username, $password, $dbname);
+            $conn = new mysqli($servername, $username, $password, $dbname);
             
             // Check connection 
             if ($conn->connect_error) 
