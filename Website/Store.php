c7d31695f79e31d42e20b059b2bbac0cf1ee6cfb
ShaunHarmon <shaun.harmon@gmail.com>
2018-10-21 00:08:52 -0500

    up

--- Website/Store.php
+++ Website/Store.php
@@ -28,7 +28,7 @@
             //Allow Headers 
             //header('Access-Control-Allow-Origin: *');
             //$servername = "localhost:3306"; 
-            $servername = "40.113.207.35:3306";
+            $servername = "sh7652server.centralus.cloudapp.azure.com:3389";
             $username = "manager1"; 
             $password = "July201998!!"; 
             $dbname = "products";
